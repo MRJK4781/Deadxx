cdc22ca9c7253662505c740440f0c384de7c82f2
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2023-10-18 00:16:29 +0530

    Update song.py

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -42,7 +42,7 @@ async def song(_, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☁️ ᴛɪᴛʟᴇ : {title[:23]}\n⏱️ ᴅᴜʀᴀᴛɪᴏɴ : `{duration}`\n🥀 ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ : {app.mention}"
+        rep = f"☁️ ᴛɪᴛʟᴇ : {title[:23]}\n⏱️ ᴅᴜʀᴀᴛɪᴏɴ : <code>{duration}</code>\n🥀 ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ : {app.mention}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
